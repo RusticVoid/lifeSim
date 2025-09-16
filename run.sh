ebd92c859801a4661d108f593a2c4821075e9b92
RusticVoid <connorpearson942@gmail.com>
2025-09-16 10:24:36 -0400

    first commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+./libs/linux/love.AppImage ./src/
---
+++ src/main.lua
@@ -0,0 +1,11 @@
+-- Load some default values for our rectangle.
+function love.load()
+end
+
+-- Increase the size of the rectangle every frame.
+function love.update(dt)
+end
+
+-- Draw a coloured rectangle.
+function love.draw()
+end
